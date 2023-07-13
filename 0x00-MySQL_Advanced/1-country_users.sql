5b54a2295f38a6825205ccae693af0001757311e
samsonkimani <samsonkimani43@gmail.com>
2023-07-13 13:36:55 +0300

    update the users table

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -2,12 +2,9 @@
 -- using enum in table columns
 
 -- Check if the table already exists
-IF NOT EXISTS (SELECT * FROM information_schema.tables WHERE table_name = 'users') THEN
-    -- Create the table
-    CREATE TABLE users (
-        id SERIAL PRIMARY KEY,
+CREATE TABLE users (
+	id INTEGER NOT NULL SERIAL PRIMARY KEY AUTO_INCREMENT,
         email VARCHAR(255) NOT NULL UNIQUE,
         name VARCHAR(255),
         country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
-    );
-END IF;
+	);
