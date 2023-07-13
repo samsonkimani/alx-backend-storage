7a449eb9789ac888398074ae3021874bb2c0c36a
samsonkimani <samsonkimani43@gmail.com>
2023-07-13 13:43:43 +0300

    update the users table

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -3,7 +3,7 @@
 
 -- Check if the table already exists
 CREATE TABLE IF NOT EXISTS users (
-	id INTEGER NOT NULL SERIAL PRIMARY KEY AUTO_INCREMENT,
+	id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
         email VARCHAR(255) NOT NULL UNIQUE,
         name VARCHAR(255),
         country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
