0125d5493ae08a392d99273acc732b0c025dbc02
samsonkimani <samsonkimani43@gmail.com>
2023-07-13 12:30:27 +0300

    creating users table

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,8 @@
+-- a script to create a table users
+-- with attributes id, email and name
+
+CREATE TABLE IF NOT EXISTS users(
+	id INTEGER PRIMARY KEY AUTO_INCREMENT,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255)
+	);
