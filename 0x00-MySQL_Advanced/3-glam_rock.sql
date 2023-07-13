82b4ecc9e7efd6291888ad486020d109613d784c
samsonkimani <samsonkimani43@gmail.com>
2023-07-13 17:01:17 +0300

    getting the lifespan of a band

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,8 +1,11 @@
-CREATE TEMPORARY TABLE glam_rock AS(
-	SELECT band_name,
-	2022 - COALESCE(CAST(SUBSTRING_INDEX(split, '-', 1) AS UNSIGNED), formed) AS lifespan
-	FROM metal_bands
-	WHERE FIND_IN_SET("Glam rock", style) > 0
+-- creating an sql file to that gets the lifetime of a band
+CREATE TEMPORARY TABLE glam_rock_bands AS (
+  SELECT band_name, 2022 - formed AS lifespan
+  FROM metal_bands
+  WHERE FIND_IN_SET('Glam rock', style) > 0
 );
 
-SELECT band_name, lifespan FROM glam_rock ORDER BY lifespan DESC;
+-- Select and display the rankings
+SELECT band_name, lifespan
+FROM glam_rock_bands
+ORDER BY lifespan DESC
