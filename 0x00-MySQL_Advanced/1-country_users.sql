11385f7866f0c90aa3291a9bf6cbd9b1758bf68e
samsonkimani <samsonkimani43@gmail.com>
2023-07-13 13:37:51 +0300

    update the users table

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -2,7 +2,7 @@
 -- using enum in table columns
 
 -- Check if the table already exists
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
 	id INTEGER NOT NULL SERIAL PRIMARY KEY AUTO_INCREMENT,
         email VARCHAR(255) NOT NULL UNIQUE,
         name VARCHAR(255),
