41f08c0845a3d9ec94460906e09d72d426f96cee
samsonkimani <samsonkimani43@gmail.com>
2023-07-18 18:21:06 +0300

    printing logs in mongodb

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -11,6 +11,7 @@ if __name__ == "__main__":
     collection = client.logs.nginx
 
     print(f"{collection.count_documents({})} logs")
+    print("Methods:")
 
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
