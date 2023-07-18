4298274a3dc4080c8cb16ff5e5602f4c38b7aa0b
samsonkimani <samsonkimani43@gmail.com>
2023-07-18 18:28:18 +0300

    printing logs in mongodb

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,26 +1,31 @@
 #!/usr/bin/env python3
 
-"""
-getting logs
-"""
-
+""" getting logs"""
 from pymongo import MongoClient
-if __name__ == "__main__":
 
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    collection = client.logs.nginx
 
-    print(f"{collection.count_documents({})} logs")
-    print("Methods:")
+def log_stats():
+    """ log stats"""
+    client = MongoClient('mongodb://localhost:27017')
+
+    db = client.logs
+    collection = db.nginx
+
+    total_logs = collection.count_documents({})
+
+    print(f"{total_logs} logs")
 
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
         count = collection.count_documents({"method": method})
         print(f"    method {method}: {count}")
 
-    # Count documents with specific method and path
     specific_method = "GET"
     specific_path = "/status"
     specific_count = collection.count_documents(
         {"method": specific_method, "path": specific_path})
     print(f"{specific_count} status check")
+
+
+if __name__ == "__main__":
+    log_stats()
